1df7e47487ac547cbc2a4008fcbc481953db0b1d
Qiyu Yan <3437889+karuboniru@users.noreply.github.com>
2020-04-13 17:56:06 +0800

    commit

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -12,8 +12,8 @@
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-std::vector <std::string> elasticlist{"CoulombScat", "hadElastic", "msc"};
-std::vector <std::string> inelasticlist{"hIoni", "protonInelastic", "hBrems", "hPairProd"};
+std::vector<std::string> elasticlist{"CoulombScat", "hadElastic", "msc"};
+std::vector<std::string> inelasticlist{"hIoni", "protonInelastic", "hBrems", "hPairProd"};
 
 SteppingAction::SteppingAction(EventAction *eventAction)
     : G4UserSteppingAction(),
@@ -35,20 +35,27 @@ void SteppingAction::UserSteppingAction(const G4Step *step)
   if (step->GetTrack()->GetTrackID() == 1)
   {
     fEventAction->AddEdep(step->GetTrack()->GetStepLength());
-    if(step->GetPreStepPoint()->GetProcessDefinedStep() != nullptr){
-    
+    if (step->GetPreStepPoint()->GetProcessDefinedStep() != nullptr)
+    {
+
       // G4cout << step->GetPreStepPoint()->GetKineticEnergy()/MeV<< "\t" << step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessName() << "\t" <<  step->GetPostStepPoint()->GetKineticEnergy()/MeV << G4endl;
       // G4cout << step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessSubType() << G4endl;
-      if (std::any_of(elasticlist.begin(), elasticlist.end(),[&](auto &str){return str == step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessName();}))
+      if (std::any_of(elasticlist.begin(), elasticlist.end(), [&](auto &str) { return str == step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessName(); }))
       {
         fEventAction->addel(1);
       }
-      else if(std::any_of(inelasticlist.begin(), inelasticlist.end(), [&](auto str){return str == step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessName();}))
+      else if (std::any_of(inelasticlist.begin(), inelasticlist.end(), [&](auto str) { return str == step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessName(); }))
       {
         fEventAction->addnel(1);
       }
+      if (step->GetPostStepPoint()->GetKineticEnergy() / MeV < 0.01)
+      {
+        if (step->GetPreStepPoint()->GetProcessDefinedStep()->GetProcessName() == "protonInelastic")
+          fEventAction->setel(1);
+        else
+          fEventAction->setel(0);
+      }
     }
-    fEventAction->setel(1);
   }
 }
 
